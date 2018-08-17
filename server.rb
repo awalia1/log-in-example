f09a44b5bb3ce4b713773caeb0aa0e42cbb44823
Amarpreet Walia <amarpreet.s.walia@gmail.com>
2018-08-17 14:57:29 -0400

    added to server and views

--- server.rb
+++ server.rb
@@ -1,6 +1,38 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'securerandom'
+enable :sessions
 
 set :database, "sqlite3:log-in.sqlite3"
+
+get "/" do 
+	p "Sinatra is running"
+end
+
+get "/login" do 
+	erb :login
+end
+
+post "/login" do
+	@email = params["email"]
+	@password = params["password"]
+
+	p "Your email is, #{params['email']}"
+	p "Your email is, #{params['password']}"
+
+	redirect :account
+end
+
+get '/account' do
+ 	erb :account
+end
+
+get '/signup' do
+ 	erb :signup
+end
+
+post '/signup' do
+ 	p params
+end
 
 require './models'
